fd9256630da6abc4a7953135addb630a4520f020
Hal Wilkinson <hal.wilkinson@optimumenergyco.com>
2020-12-03 09:50:01 -0800

    finished day3 part 2

--- day3/spec/tree_counter_spec.rb
+++ day3/spec/tree_counter_spec.rb
@@ -4,7 +4,7 @@ describe 'TreeCounter#count_trees' do
   context 'when given my sample' do
     it 'counts the number of trees' do
       expect(
-        TreeCounter.count_trees('spec/sample_input.txt')
+        TreeCounter.count_trees('spec/sample_input.txt', [[3, 1]])
       ).to eq(1)
     end
   end
@@ -12,7 +12,7 @@ describe 'TreeCounter#count_trees' do
   context 'when given the aoc sample' do
     it 'counts the number of trees' do
       expect(
-        TreeCounter.count_trees('spec/aoc_sample_input.txt')
+        TreeCounter.count_trees('spec/aoc_sample_input.txt', [[3, 1]])
       ).to eq(7)
     end
   end
@@ -20,8 +20,26 @@ describe 'TreeCounter#count_trees' do
   context 'when given the aoc sample' do
     it 'counts the number of trees' do
       expect(
-        TreeCounter.count_trees('spec/puzzle_input.txt')
+        TreeCounter.count_trees('spec/puzzle_input.txt', [[3, 1]])
       ).to eq(169)
+    end
+  end
+
+  # part 2
+
+  context 'when given the aoc sample and the array of slopes' do
+    it 'counts the number of trees' do
+      expect(
+        TreeCounter.count_trees('spec/aoc_sample_input.txt', [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]])
+      ).to eq(336)
+    end
+  end
+
+  context 'when given my puzzle input and the array of slopes' do
+    it 'counts the number of trees' do
+      expect(
+        TreeCounter.count_trees('spec/puzzle_input.txt', [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]])
+      ).to eq(7_560_370_818)
     end
   end
 end
--- day3/tree_counter.rb
+++ day3/tree_counter.rb
@@ -3,16 +3,17 @@ require 'benchmark'
 
 module TreeCounter
   class << self
-    def count_trees(file_path)
+    def count_trees(file_path, slopes)
       file = File.open(file_path)
       file_data = file.readlines.map(&:chomp)
       file.close
 
       grid = file_data.map { |string| string.chars.to_a }
-      sum_trees(grid)
+
+      slopes.map { |right, down| sum_trees(grid, right, down) }.inject(:*)
     end
 
-    def sum_trees(grid)
+    def sum_trees(grid, right, down)
       width = grid[0].length
       num_rows = grid.length
       sum = 0
@@ -20,8 +21,8 @@ module TreeCounter
       col_index = 0
 
       loop do
-        col_index = (col_index + 3) % width
-        row_index += 1
+        col_index = (col_index + right) % width
+        row_index += down
 
         return sum if row_index >= num_rows
 
