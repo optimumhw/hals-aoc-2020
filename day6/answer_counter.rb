18fcb07e0394ceff84d9f45d9d27c0a619042b97
Hal Wilkinson <hal.wilkinson@optimumenergyco.com>
2020-12-06 17:45:52 -0800

    finished day6

---
+++ day6/answer_counter.rb
@@ -0,0 +1,44 @@
+require 'pry'
+require 'benchmark'
+require 'json'
+
+module AnswerCounter
+  class << self
+    def count_answers(file_path)
+      file_data = File.read(file_path)
+
+      file_data
+        .split(/\n{2,}/)
+        .map { |blob| blob.split(/\n/) }
+        .map { |group| group.join}
+        .map do |group|
+        group.scan(/\w/).each_with_object(Hash.new(0)) { |i,h| h[i] += 1; }.length
+      end.inject(:+)
+
+    end
+
+    # part 2 ===========================
+    def count_yes_answers_to_all_questions(file_path)
+      file_data = File.read(file_path)
+
+      groups = file_data
+        .split(/\n{2,}/)
+        .map { |blob| blob.split(/\n/) }
+
+      group_sizes = groups.map{ |group| group.length }
+
+      group_answers = groups.map { |group| group.join}
+      .map do |group|
+        group.scan(/\w/).each_with_object(Hash.new(0)) { |i,h| h[i] += 1; }
+      end
+
+      group_answers[2].select {|key, value| value == group_sizes[2]}
+
+      group_sizes.each_with_index.map do |count, idx|
+        group_answers[idx].select {|key, value| value == group_sizes[idx]}.length
+      end
+      .inject(:+)
+
+    end
+  end
+end
---
+++ day6/spec/answer_counter_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../answer_counter'
+
+describe 'AnswerCounter#count_valid_passports' do
+  context 'when given the sample input' do
+    it 'counts the answers' do
+      expect(
+        AnswerCounter.count_answers('spec/sample_input.txt')
+      ).to eq(11)
+    end
+  end
+
+  context 'when given my part1 input' do
+    it 'counts the answers' do
+      expect(
+        AnswerCounter.count_answers('spec/puzzle_input.txt')
+      ).to eq(6583)
+    end
+  end
+
+  # part2 ------------------------------
+
+  context 'when given the sample input' do
+    it 'counts yes answers to all questions' do
+      expect(
+        AnswerCounter.count_yes_answers_to_all_questions('spec/sample_input.txt')
+      ).to eq(6)
+    end
+  end
+
+  context 'when given my part1 input' do
+    it 'counts yes answers to all questions' do
+      expect(
+        AnswerCounter.count_yes_answers_to_all_questions('spec/puzzle_input.txt')
+      ).to eq(3290)
+    end
+  end
+
+end
