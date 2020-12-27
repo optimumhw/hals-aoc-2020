cc4743c9596630c697685411fc9128451cd975c7
Hal Wilkinson <hal.wilkinson@optimumenergyco.com>
2020-12-27 07:15:00 -0800

    finished day14 part2

--- day14/memory_puzzle.rb
+++ day14/memory_puzzle.rb
@@ -25,9 +25,49 @@ module MemoryPuzzle
     end
 
     def apply_mask(mask, number)
-      or_mask = mask.gsub(/[X]/, '0').to_i(2)
-      and_mask = mask.gsub(/[X]/, '1').to_i(2)
+      or_mask = mask.gsub('X', '0').to_i(2)
+      and_mask = mask.gsub('X', '1').to_i(2)
       (number | or_mask) & and_mask
+    end
+
+    # part 2 =========================================
+
+    def change_memory(file_path)
+      values = IO.readlines(file_path, chomp: true).map(&:to_s)
+
+      arr = values.map { |v| v.gsub(/\s+/, '').split('=') }
+
+      memory = {}
+      mask = ''
+
+      arr.each do |pair|
+        if pair[0] == 'mask'
+          mask = pair[1]
+        else
+          start_address = pair[0].match(/(\d+)/).captures[0].to_i
+          locations = addresses(mask, start_address)
+
+          locations.map { |a| memory.store(a, pair[1].to_i)}
+
+        end
+      end
+      memory.map { |_k, v| v }.reduce(0) { |sum, v| sum + v }
+    end
+
+    def addresses(mask, start_address)
+      zeros_for_xs = mask.gsub('X', '0').to_i(2)
+      and_mask = mask.gsub('0', '1').gsub('X', '0').to_i(2)
+
+      mask_applied = start_address | zeros_for_xs
+      final = mask_applied & and_mask
+
+      float_mask = mask.gsub('1', '0').gsub('X', '1').to_i(2)
+      twos = (0..35).map { |n| (2**(35 - n) & float_mask) }.select { |n| n.positive? }
+
+      combos = (0..twos.length).map { |n| twos.combination(n) }
+      or_masks = combos.map { |z| z.map { |q| q}}.flatten(1).map { |a| a.reduce(0) { |s, v| s + v}}
+
+      or_masks.map { |n| (n | final)}
     end
   end
 end
--- day14/spec/memory_puzzle_spec.rb
+++ day14/spec/memory_puzzle_spec.rb
@@ -26,4 +26,30 @@ describe 'MemoryPuzzle#mem_sum' do
       ).to eq(10_035_335_144_067)
     end
   end
+
+  # ===  part2 ==============================
+
+  context 'when given the part2 sample' do
+    it 'computes the sum' do
+      expect(
+        MemoryPuzzle.change_memory('spec/part2_sample.txt')
+      ).to eq(208)
+    end
+  end
+
+  context 'when given the part2 other sample' do
+    it 'computes the sum' do
+      expect(
+        MemoryPuzzle.change_memory('spec/other_sample.txt')
+      ).to eq(185_941_754_368)
+    end
+  end
+
+  context 'when given the part2 puzzle input' do
+    it 'computes the sum' do
+      expect(
+        MemoryPuzzle.change_memory('spec/puzzle_input.txt')
+      ).to eq(3_817_372_618_036)
+    end
+  end
 end
